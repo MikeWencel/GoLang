c04b999c97b88e9ab1e560592b913a883c13a7db
MikeWencel <wencel.michael@gmail.com>
2020-09-02 17:34:37 +0200

    first steps with control flow

--- FirstSection/Hello/main.go
+++ FirstSection/Hello/main.go
@@ -4,4 +4,14 @@ import "fmt"
 
 func main() {
 	fmt.Println("Hello!!!")
+	foo()
 }
+
+func foo() {
+	fmt.Println("I'm in foo")
+}
+
+// Control Flow:
+// (1) sequence
+// (2) loop; iterative
+// (3) conditional
