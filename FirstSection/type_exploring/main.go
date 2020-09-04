eca5fc14dc66b4c560913f820a1703d1fb675324
MikeWencel <wencel.michael@gmail.com>
2020-09-04 19:33:29 +0200

    You're doing greatgit add .!

--- FirstSection/type_exploring/main.go
+++ FirstSection/type_exploring/main.go
@@ -5,10 +5,14 @@ import (
 )
 
 var y = 99
-var b = "Few words"
+// We DECLARE that Z is STRING
+var z = "Few words"
 
 func main() {
 	fmt.Println(y)
 	fmt.Printf("%T\n", y)
+	fmt.Println(z)
+	fmt.Println("%T\n", z)
+
 
 }
