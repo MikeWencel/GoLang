15cf3843c51f6c9bf9c45b2b7bf7094c2bd503c3
MikeWencel <wencel.michael@gmail.com>
2020-09-14 18:54:28 +0200

    You're doing great!

--- second_section/strings/main.go
+++ second_section/strings/main.go
@@ -6,6 +6,7 @@ import (
 
 var x int
 var y float64
+var z byte
 
 func main() {
 	s := "Michał i Kasia Wencel"
@@ -14,6 +15,10 @@ func main() {
 	bs := []byte(s)
 	fmt.Println(bs)
 	fmt.Printf("%T\n", bs)
+	z := 102
+	bz := string(z)
+	fmt.Println(bz)
+	fmt.Printf("%T\n", bz)
 
 	for i := 0; i < len(s); i++ {
 		fmt.Printf("%#U ", s[i])
